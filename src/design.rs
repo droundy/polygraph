15d47faaf04b109b2147dd5a7af164aa005df568
David Roundy <daveroundy@gmail.com>
2021-12-13 05:50:08 -0800

    Flesh out a bit of implementation for Ref types.

--- src/design.rs
+++ src/design.rs
@@ -1,5 +1,17 @@
 //! Ideas for what a generated API might look like.
 
+#[drive(Copy, Clone, PartialEq, Eq, Hash)]
+pub struct Key<T> {
+  index: usize,
+  phantom: PhantomData<T>,
+}
+
+#[drive(Clone, PartialEq, Eq, Hash)]
+pub struct KeySet<T> {
+  set: SetUsize,
+  phantom: PhantomData<Key<T>>,
+}
+
 polygraph!{
   struct Person {
      /// Biological father
@@ -15,35 +27,63 @@ polygraph!{
 }
 
 /// The above generates:
-struct PersonDatum {
+struct Person {
      /// Biological father
      father: Option<Key<Person>>,
+     #[serde(ignore)]
      father_of: Set<Person>,
      /// Biological mother
      mother: Option<Key<Person>>,
+     #[serde(ignore)]
      mother_of: Set<Person>,
      godparents: Set<Person>,
+     #[serde(ignore)]
      godparent_of: Set<Person>,
      surname: Surname,
      givenname: String,
 }
 
-struct SurnameDatum {
+struct Surname {
   value: String,
+  #[serde(ignore)]
   persons: Set<Person>,
 }
 
-struct Graphs {
-  persons: Vec<PersonDatum>,
-  surnames: Vec<SurnameDatum>,
+#[drive(Clone, PartialEq, Eq, Hash, Serializes, Deserialize)]
+pub struct Graphs {
+  person: Vec<PersonDatum>,
+  surname: Vec<SurnameDatum>,
 }
 
+#[drive(Clone, PartialEq, Eq, Hash)]
+pub struct RefSet<'a, T> {
+  graphs: &'a Graphs,
+  set: &'a KeySet<T>,
+}
+#[drive(Copy, Clone, PartialEq, Eq, Hash)]
 pub struct Ref<'a, T> {
   graphs: &'a Graphs,
   key: Key<T>,
 }
+impl<'a> Deref<Person> Ref<'a, Person> {
+  fn deref(& self) -> & Person {
+    &self.graphs.person[self.key.index]
+  }
+}
 impl<'a> Ref<'a, Person> {
-  fn father_of(& self) -> SetRef<Person> {}
+  fn father_of(&self) -> SetRef<Person> {
+    RefSet {
+      graphs: self.graphs,
+      set: &self.graphs.person[self.key.index].father_of,
+    }
+  }
+  fn father(&self) -> Option<Ref<Person>> {
+    self.graphs.person[self.key.index].father.map(|key|
+    Ref {
+      graphs: self.graphs,
+      key,
+    })
+  }
 }
 
 
